5aed952daa8e6b7b7479e5887ce843174a432050
Barso2 <reznitskyalex@gmail.com>
2023-02-16 03:13:56 +0200

    Add hoursFrom/To selectors

--- src/ui/data-form.js
+++ src/ui/data-form.js
@@ -1,12 +1,14 @@
 const FORM_ID = "data-form-id";
 const DATE_FROM_ID = "date-from-id";
 const DATE_TO_ID = "date-to-id"
-const HOUR_ID = "hour-id"
+const HOUR_FROM_ID = "hour-from-id"
+const HOUR_TO_ID = "hour-to-id"
 export class DataForm {
     #formElement;
     #dateFromElement;
     #dateToElement;
-    #hoursId
+    #hoursFromId
+    #hoursToId;
     //       "form-section"  16
     constructor(parentId, maxDays) {
         const parentElement = document.getElementById(parentId);
@@ -20,24 +22,30 @@ export class DataForm {
         parentElement.innerHTML = `<form id="${FORM_ID}">
             <input type="date" id="${DATE_FROM_ID} required">
             <input type="date" id="${DATE_TO_ID}" required>
-            <select id="${HOUR_ID}">
-                    <option value="kkk">kkk</option>
-                </select>
+            <select id="${HOUR_FROM_ID}">
+                <option value="kkk">kkk</option>
+            </select>
+            <select id="${HOUR_TO_ID}">
+                <option value="kkk">kkk</option>
+            </select>
             <button type="submit">Submit</button>
             <button type="reset">Reset</button>
         </form>`
-        this.#hoursId = document.getElementById(HOUR_ID)
-        this.setHourOptions();
+        this.#hoursFromId = document.getElementById(HOUR_FROM_ID)
+        this.#hoursToId = document.getElementById(HOUR_TO_ID)
+        this.setHourOptions(this.#hoursFromId, "Hours from");
+        this.setHourOptions(this.#hoursToId, "Hours to");
     }
 
-    setHourOptions() {
-        let res;
-        for (let i = 0; i !== 24; i++) {
-            res += (`<option value="${i}">${i}</option>`.join(""))
+    setHourOptions(hours, str) {
+        let res = `<select id="${HOUR_TO_ID}">
+        <option value="" disabled selected>${str}</option>
+    </select>`;
+        for (let i = 0; i < 24; i++) {
+            res += (`<option value="${i}">${i + ":00"}</option>`)
+            console.log(res)
         }
-        return this.#hoursId.innerHTML = res;
-
-
+        return hours.innerHTML = res;
     }
     #setMinMaxDates(maxDays) {
         const current = new Date();//Wed Feb 15 2023 23:08:46 GMT+0200 (Israel Standard Time)
